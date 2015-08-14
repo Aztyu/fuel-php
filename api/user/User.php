778110802463e0e4c57f8449d5a5a0ff9c28a497
cBeal <169796@supinfo.com>
2015-08-14 22:44:28 +0200

    Add user class and move connection.php

---
+++ api/user/User.php
@@ -0,0 +1,288 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aztyu
+ * Date: 05/08/2015
+ * Time: 19:54
+ */
+
+class User {
+    private $id;
+    private $pseudo;
+    private $email;
+    private $firstname;
+    private $lastname;
+    private $car_id;
+
+    private $password;
+    private $salt;
+    private $hash;
+    private $token;
+
+    public static function isPseudoTaken($bdd, $pseudo){
+        $request = $bdd->prepare('SELECT pseudo FROM driver WHERE pseudo = ?');
+        $request->execute(array($pseudo));
+
+        if($donnees = $request->fetch()){
+            return True;
+        }else{
+            return False;
+        }
+    }
+
+    private static function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public static function isEmailTaken($bdd, $email){
+        $request = $bdd->prepare('SELECT pseudo FROM driver WHERE email = ?');
+        $request->execute(array($email));
+
+        if($donnees = $request->fetch()){
+            return True;
+        }else{
+            return False;
+        }
+    }
+
+    public static function getUser($bdd, $hash, $pseudo = NULL, $email = NULL){
+        if($pseudo){
+            $request = $bdd->prepare('SELECT * FROM driver WHERE pseudo = ?');
+            $request->execute(array($pseudo));
+
+            if($donnees = $request->fetch()){
+                $salt = $donnees["salt"];
+                var_dump(hash('sha512', $salt + $hash, false));
+                var_dump($donnees["hash"]);
+
+                if($donnees["hash"] == hash('sha512', $salt + $hash, false)){
+                    $user = new User($pseudo, $donnees["hash"], $donnees["email"]);
+                    $user->setId($donnees["driver_id"]);
+                    $user->setCarId($donnees["car_id"]);
+                    $user->setPseudo($donnees["pseudo"]);
+                    $user->setFirstname($donnees["first_name"]);
+                    $user->setLastname($donnees["last_name"]);
+                    $user->setToken(User::generateRandomString(200));
+
+                    return $user;
+                }
+            }else{
+                return null;
+            }
+        }else if($email){
+            $request = $bdd->prepare('SELECT * FROM driver WHERE email = ?');
+            $request->execute(array($email));
+
+            if($donnees = $request->fetch()){
+                $salt = $donnees["salt"];
+                var_dump(hash('sha512', $salt + $hash, false));
+                var_dump($donnees["hash"]);
+
+                if($donnees["hash"] == hash('sha512', $salt + $hash, false)){
+                    $user = new User($donnees["pseudo"], $donnees["hash"], $email);
+                    $user->setId($donnees["id"]);
+                    $user->setCarId($donnees["car_id"]);
+                    $user->setFirstname($donnees["first_name"]);
+                    $user->setLastname($donnees["last_name"]);
+                    $user->setToken(generateRandomString(200));
+
+                    return $user;
+                }
+            }else{
+                return null;
+            }
+        }else{
+            return null;
+        }
+    }
+
+    public function __construct($pseudo, $pwd, $email) {
+        $this->pseudo = $pseudo;
+        $this->password = $pwd;
+        $this->email = $email;
+    }
+
+    public function copyObject($user){
+        $this->pseudo = $user->pseudo;
+        $this->password = $user->password;
+        $this->email = $user->email;
+    }
+
+    public function addToDatabase()
+    {
+        $bdd = ConnectToMySQL();
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        if ($this->isPseudoTaken($bdd, $this->pseudo)) {
+            $result = 'Le pseudo ' . $this->pseudo . ' est déjà pris';
+            Message::sendJSONMessage(true, $result);
+        }else if($this->isEmailTaken($bdd, $this->email)){
+            $result = 'Le mail ' . $this->email . ' est déjà pris';
+            Message::sendJSONMessage(true, $result);
+        }else{
+            try {
+                $this->hashPassword();
+
+                $request = $bdd->prepare("INSERT INTO driver (pseudo, hash, salt, email) VALUES(:pseudo, :hash, :salt, :email)");
+                $request->execute(array(
+                    'pseudo' => $this->pseudo,
+                    'hash' => $this->hash,
+                    'salt' => $this->salt,
+                    'email' => $this->email
+                ));
+
+                $request->closeCursor();
+
+                $result = "Utilisateur rajouté";
+                Message::sendJSONMessage(false, $result);
+            }catch(Exception $e){
+                die('Erreur : '.$e->getMessage());
+                $result = "Erreur interne";
+                Message::sendJSONMessage(true, $result);
+            }
+        }
+    }
+
+    public function updateToDatabase(){
+
+    }
+
+    private function hashPassword(){
+        $this->salt = hash('sha512', $this->generateRandomString(), false);
+        $this->hash = hash('sha512', $this->salt + $this->password, false);
+    }
+
+    public function asArray(){
+        return array("id" => $this->id,
+            "pseudo" => $this->pseudo,
+            "firstname" => $this->firstname,
+            "lastname" => $this->lastname,
+            "email" => $this->email,
+            "car_id" => $this->car_id,
+            "token" => $this->token);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * @param mixed $pseudo
+     */
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param mixed $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCarId()
+    {
+        return $this->car_id;
+    }
+
+    /**
+     * @param mixed $car_id
+     */
+    public function setCarId($car_id)
+    {
+        $this->car_id = $car_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+
+}
+
+?>
---
+++ api/user/create.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aztyu
+ * Date: 05/08/2015
+ * Time: 19:47
+ */
+
+include('../../database/connection.php');
+include('../../request/Message.php');
+include('User.php');
+
+header ('Content-type: text/html; charset=utf-8');
+
+$parse_url = array();
+
+$arg_count = 0;
+
+foreach($_REQUEST as $key => $value){
+    $buffer = explode("=", $value);
+    $parse_url[$arg_count][0] = $key;
+    $parse_url[$arg_count][1] = $value;
+    $arg_count++;
+}
+
+switch(count($parse_url)){
+    case 3:
+        if($parse_url[0][0] == 'pseudo' && $parse_url[1][0] == 'password' && $parse_url[2][0] == 'email'){
+            CreateUser($parse_url[0][1], $parse_url[1][1], $parse_url[2][1]);
+        }
+        break;
+    default:
+        Message::sendJSONMessage(false, "Requete invalide");
+        break;
+}
+
+function CreateUser($pseudo, $pwd, $email){
+    $new_user = new User($pseudo, $pwd, $email);
+    $new_user->addToDatabase();
+}
+
+?>
